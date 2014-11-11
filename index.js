a3af8583bc2c7b632d7d352c44ab5bba73b72fbe
Alexey <petrovi4@gmail.com>
2014-11-11 21:54:47 +0300

    Fast fix

--- index.js
+++ index.js
@@ -3,80 +3,39 @@ var nlog = require('fly-nlog'),
 	exec = require('child_process').exec,
 	_ = require('lodash');
 
-//nconf.load(function () {
-//
-//	//CONFIG.zabbix.host = 'zabbix.dev.fly.me';
-//	//exports.push('3ds_authorize_declinded_by_antifrod-filter');
-//
-//});
-
 var hostname = null;
 
 exec('hostname -f', function (error, _hostname) {
 	hostname = _hostname.replace(/[\n\r]+/g, '');
-
-	//nlog.info({
-	//	message: 'FLY_ZABBIX:HOSTNAME',
-	//	hostname: _hostname
-	//});
 });
 
 exports.push = function (data) {
 
-	// exec('hostname -f', function (error, _hostname) {
-	// 	hostname = _hostname.replace(/[\n\r]+/g, '');
+		var command = ['zabbix_sender -vv',
+			"-s " + hostname,
+			'-z '+CONFIG.zabbix.host,
+		];
 
-		var command = ['zabbix_sender -vv -s',
-				hostname,
-				'-z '+CONFIG.zabbix.host+' -k',
-				metric,
-				'-o'];
-		var commandStr;
+		var dataArr = _.isArray(data) ? data : [data];
 
-		if (_.isArray(data)) {
+		_.each(data, function (metric) {
 
-			_.each(data, function (metric) {
+			if (_.isString(metric)) {
 
-				if (_.isString(metric)) {
-
-					command.push(1);
-					commandStr = command.join(' ');
-
-				} else {
-
-					command.push(metric.hasOwnProperty('value') ? metric.value : 1);
-					command = command.join(' ');
-
-				}
-
-				exec(command, function (error, data) {
-					console.log(error);
-					console.log(data);
-				});
-
-			});
-
-		} else {
-
-			if (_.isString(data)) {
-
-					command.push(1);
-					commandStr = command.join(' ');
+				command.push("-k " + metric);
+				command.push('-o ' + 1);
 
 			} else {
 
-					command.push(metric.hasOwnProperty('value') ? metric.value : 1);
-					command = command.join(' ');
-
+				command.push("-k " + metric.key);
+				command.push('-o ' + metric.hasOwnProperty('value') ? metric.value : 1);
+				
 			}
 
-			exec(commandStr, function (error) {
+			exec(command.join(' '), function (error, data) {
 				console.log(error);
 				console.log(data);
 			});
-
-		}
-
-	// });
+		});
 
 };
